f22553d84bfbd33e9d94d5724c87e14e0dda852d
joshbenjamin <joshbenjamin13@gmail.com>
2016-10-19 11:20:03 +0200

    Add files via upload

--- BOBTR2016.py
+++ BOBTR2016.py
@@ -2,22 +2,43 @@ from flask import Flask, redirect, url_for, render_template, json, request
 
 app = Flask(__name__)
 
-@app.route('/')                           #Just reroutes the default address to the welcome page
+@app.route('/')                           # Just reroutes the default address to the welcome page
 def index():
     return redirect(url_for('welcome'))
 
 @app.route('/Welcome')
 def welcome():
-    options = ["Sex", "Class", "Age"]  # Options for data to be viewed
+    options = ["Sex", "Class", "Age"]     # Options for data to be viewed
     return render_template('welcome.html', options=options)
 
-@app.route('/data/<category>')
+@app.route('/data/<category>', methods=['GET', 'POST'])
 def front(category):
-    total = 0
-    count = 0
+    names = []
+    num_entries = len(request.json)
 
-    #name = request.json['name']
+    if category == 'Sex':
+        num_males = 0
+        num_females = 0
+        survived_males = 0
+        survived_females = 0
 
+        for i in range(0, num_entries):
+            if(request.json[i]['sex'] == "male"):
+                num_males+=1
+                if(request.json[i]['survived'] == "1"):
+                    survived_males+=1
+            else:
+                num_females+=1
+                if (request.json[i]['survived'] == "1"):
+                    survived_females += 1
+
+
+    #print ("Total: ", num_entries, "\n", "Males: ", num_males, "\n", "Females: ", num_females)
+    print "Total: ", num_entries
+    print "Males: ", num_males
+    print "Number of Survivors: ", survived_males
+    print "Females: ", num_females
+    print "Number of Survivors: ", survived_females
 
     return render_template('data.html', category=category)
 
